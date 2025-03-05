de3ff4a8bae470b1bdb86b2e7e86270364e91c5c
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-05 15:21:34 +0100

    added more options, rules and groups for input fields

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -4,40 +4,69 @@ export const eventType = defineType({
   name: 'event',
   title: 'Event',
   type: 'document',
+  groups: [
+    {name: 'details', title: 'Details'},
+    {name: 'editorial', title: 'Editorial'},
+  ],
   fields: [
     defineField({
       name: 'name',
       type: 'string',
+      group: 'details',
     }),
     defineField({
       name: 'slug',
       type: 'slug',
+      options: {source: 'name'},
+      validation: (rule) => rule.required().error('Required to generate a page on the website'),
+      hidden: ({document}) => !document?.name,
+      group: 'details',
     }),
     defineField({
       name: 'eventType',
       type: 'string',
+      options: {
+        list: ['in-person', 'virtual'],
+        layout: 'radio',
+      },
+      group: 'details',
     }),
     defineField({
       name: 'date',
       type: 'datetime',
+      group: 'details',
     }),
     defineField({
       name: 'doorsOpen',
+      description: 'Number of minutes before the start time for admission',
       type: 'number',
+      initialValue: 60,
+      group: 'details',
     }),
     defineField({
       name: 'venue',
       type: 'reference',
       to: [{type: 'venue'}],
+      group: 'details',
+      readOnly: ({value, document}) => !value && document?.eventType === 'virtual',
+      validation: (rule) =>
+        rule.custom((value, context) => {
+          if (value && context?.document?.eventType === 'virtual') {
+            return 'Only in-person events can have a venue'
+          }
+          return true
+        }),
     }),
     defineField({
       name: 'headline',
       type: 'reference',
       to: [{type: 'artist'}],
+      group: 'details',
     }),
     defineField({
       name: 'image',
       type: 'image',
+      group: 'editorial',
     }),
     defineField({
       name: 'details',
@@ -47,10 +76,12 @@ export const eventType = defineType({
           type: 'block',
         }),
       ],
+      group: 'editorial',
     }),
     defineField({
       name: 'tickets',
       type: 'url',
+      group: 'details',
     }),
   ],
 })
