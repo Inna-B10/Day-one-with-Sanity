d43be8b638c9b05cba8eab42f97b3c1e3f629775
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-06 10:18:56 +0100

    added preview key, icons

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -1,9 +1,11 @@
+import {CalendarIcon} from '@sanity/icons'
 import {defineArrayMember, defineField, defineType} from 'sanity'
 
 export const eventType = defineType({
   name: 'event',
   title: 'Event',
   type: 'document',
+  icon: CalendarIcon,
   groups: [
     {name: 'details', title: 'Details'},
     {name: 'editorial', title: 'Editorial'},
@@ -12,7 +14,7 @@ export const eventType = defineType({
     defineField({
       name: 'name',
       type: 'string',
-      group: 'details',
+      group: ['details', 'editorial'],
     }),
     defineField({
       name: 'slug',
@@ -84,4 +86,32 @@ export const eventType = defineType({
       group: 'details',
     }),
   ],
+  //update the preview key in the schema
+  preview: {
+    select: {
+      name: 'name',
+      venue: 'venue.name',
+      artist: 'headline.name',
+      date: 'date',
+      image: 'image',
+    },
+    prepare({name, venue, artist, date, image}) {
+      const nameFormatted = name || 'Untitled event'
+      const dateFormatted = date
+        ? new Date(date).toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+        : 'No date'
+
+      return {
+        title: artist ? `${nameFormatted}(${artist})` : nameFormatted,
+        subtitle: venue ? `${dateFormatted} at ${venue}` : dateFormatted,
+        media: image || CalendarIcon,
+      }
+    },
+  },
 })
--- schemaTypes/venueType.ts
+++ schemaTypes/venueType.ts
@@ -1,9 +1,11 @@
+import {MarkerIcon} from '@sanity/icons'
 import {defineField, defineType} from 'sanity'
 
 export const venueType = defineType({
   name: 'venue',
   title: 'Venue',
   type: 'document',
+  icon: MarkerIcon,
   fields: [
     defineField({
       name: 'name',
