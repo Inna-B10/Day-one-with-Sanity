8f45151e59834255608dc647d9157e80b37afa27
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2025-03-06 15:39:11 +0100

    added .env parameters

--- frontend/src/sanity/client.ts
+++ frontend/src/sanity/client.ts
@@ -1,7 +1,8 @@
 import { createClient } from 'next-sanity'
 
 export const client = createClient({
-	projectId: 'j19swgii',
+	// projectId: 'j19swgii',
+	projectId: process.env.PUBLIC_NEXT_PROJECT_ID,
 	dataset: 'production',
 	apiVersion: '2024-11-01',
 	useCdn: false,
